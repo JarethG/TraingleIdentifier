6ffc60fb682975ee3b19657c5c758551fa4d2dbf
Jareth Gaskin <jarethgaskin@gmail.com>
2023-06-20 11:49:08 +1200

    logic added for traingle identification

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import express from "express";
 import postRoutes from "./routes/posts.js";
-import cookieParser from "cookie-parser";
-
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 
 
 app.use("/api/triangle/", postRoutes);
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,25 @@
 import express from "express";
-import {
-  getPosts,
-} from "../controllers/post.js";
+
+const getTriangleType = (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+  const c = req.query.c;
+
+  if (!a || !b || !c) return res.status(400).json({message: "missing parameter in query"});
+  //equilateral
+  if (a===b && b===c)
+    return res.status(200).json("EQUILATERAL");
+  //isosceles
+  else if (a===b || a===c || b===c)
+    return res.status(200).json("ISOCELES");
+  //scalene
+  else if (a!==b && b!==c)
+    return res.status(200).json("SCALENE");
+  else return res.status(500).json("this is not a recognised triangle");
+};
 
 const router = express.Router();
 
-router.get("/", getPosts);
+router.get("/", getTriangleType);
 
 export default router;
